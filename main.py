2a24ea7e3c32b0d23b23f348c45cfd842b26c834
trintrantron <catie_mae@yahoo.com>
2026-05-13 19:06:32 +0100

    draft 2

--- main.py
+++ main.py
@@ -1476,4 +1476,4 @@ def logout_student():
         return redirect("/")
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=8080, use_reloader=False)
+    socketio.run(app, debug=True, port=8080, use_reloader=False, allow_unsafe_werkzeug=True)
